89ded9fed258d39c39c79c1063469cc5f175e853
arthurbastos <arthurcoelho.27@gmail.com>
2020-12-12 13:11:03 -0300

    Foi fetio a aplicação com front end em React Js

--- src/main/App.js
+++ src/main/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Rotas from './rotas'
 import Navbar from '../components/navbar'
+import ProvedorAutenticacao from './provedorAutenticacao'
 
 import 'toastr/build/toastr.min.js'
 
@@ -18,10 +19,12 @@ class App extends React.Component{
   render(){
     return(
       <>
-        <Navbar/>
-        <div className="container">
-            <Rotas/>
-        </div>
+        <ProvedorAutenticacao>
+          <Navbar/>
+          <div className="container">
+              <Rotas/>
+          </div>
+        </ProvedorAutenticacao>
       </>
     )
   }
--- src/views/home.js
+++ src/views/home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {withRouter} from 'react-router-dom'
 
 import UsuarioService from '../app/service/usuarioService'
-import LocalStorageService from '../app/service/localstorageService'
+import {AuthContext} from '../main/provedorAutenticacao'
 
 class Home extends React.Component{
 
@@ -16,7 +16,7 @@ class Home extends React.Component{
     }
 
     componentDidMount(){
-        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
+        const usuarioLogado = this.context.usuarioAutenticado
         
         this.usuarioService
         .obterSaldoPorUsuario(usuarioLogado.id)
@@ -51,5 +51,7 @@ class Home extends React.Component{
         )
     }
 }
+
+Home.contextType = AuthContext;
 
 export default withRouter(Home) 
